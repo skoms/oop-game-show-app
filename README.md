f80c36df005df6074b9b190e8b796b807c4da948
skoms <45171368+skoms@users.noreply.github.com>
2021-01-04 21:41:55 +0100

    Add README and start Phrase class

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# oop-game-show-app
+# Phrase Hunter
  
+ ## Languages
+  * JavaScript
+  * HTML - HyperTextMarkupLanguage
+  * CSS - CascadingStyleSheets
+
+ ## Intoduction
+ This is a word guessing game where a random phrase is chosen and the player has to select the fitting letters to complete the phrase. If the player guesses it, he/she wins, if they guess incorrectly 5 times, they lose.
+
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+    constructor( phrase ) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Adds letter placeholders to the display when the game starts
+     */
+    addPhraseToDisplay() {
+        const phraseDivUl = document.querySelector('#phrase ul');
+        let phraseHTML = ``;
+
+        for (let i = 0; i < this.phrase.length; i++) {
+            const letter = this.phrase[i];
+            if( letter === ' ' ) {
+                phraseHTML += `<li class="space"> </li>`;
+            } else {
+                phraseHTML += `<li class="hide letter ${letter}">${letter}</li>`;
+            }
+        }
+
+        phraseDivUl.innerHTML = phraseHTML;
+    }
+
+    /**
+     * Checks whether or not chosen letter is in the phrase
+     * @param { string } letter - Letter chosen by the player
+     * @returns { boolean } - Whether or not it includes the letter
+     */
+    checkLetter( letter ) {
+        return this.phrase.includes(letter) && letter !== ' ';
+    }
+}
